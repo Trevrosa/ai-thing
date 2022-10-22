94502ace723d7219189c360ed34a4f27271383ff
trev <trev@trevrosa.dev>
2022-10-22 11:00:26 +0800

    Make more responsive, add cleaning dataset, removed console spam

--- main.py
+++ main.py
@@ -1,14 +1,19 @@
 import os
+import sys
 
 os.environ['TF_CPP_MIN_LOG_LEVEL'] = "2"
 
 from os.path import exists
 from os import rename, stat
-from shutil import rmtree, make_archive
+from shutil import rmtree, make_archive, move
 from statistics import mean
+from imghdr import what
+from random import uniform
+import numpy as np
 import pathlib
 import PIL
-import numpy as np
+import contextlib
+import logging
 
 import tensorflow as tf
 from tensorflow import keras
@@ -17,6 +22,7 @@ from tensorflow.keras.models import Sequential
 from tensorflow.python.ops.numpy_ops import np_config
 
 np_config.enable_numpy_behavior()
+logging.getLogger("tensorflow").setLevel(logging.ERROR)
 
 print(f"tensorflow version is {tf.__version__}, devices detected: {tf.config.list_physical_devices()}\n")
 
@@ -30,36 +36,108 @@ batch_size = 32
 img_height = 180
 img_width = 180
 
-if exists("dataset"):
-    data_dir = pathlib.Path("dataset")
-else:
+
+# noinspection PyShadowingNames
+def get_size(start_path='.'):
+    total_size = 0
+    for dirpath, dirnames, filenames in os.walk(start_path):
+        for f in filenames:
+            fp = os.path.join(dirpath, f)
+            # skip if it is a symbolic link
+            if not os.path.islink(fp):
+                total_size += os.path.getsize(fp)
+
+    return total_size
+
+
+def before_exit():
+    save = True if input("would you like to save your trained model? (y or n): ") \
+                       .lower() == "y" else False
+    if not save:
+        exit()
+
+    if exists(old_model) and exists(latest_model):
+        rmtree(old_model)
+        rename(latest_model, old_model)
+    elif exists(latest_model):
+        rename(latest_model, old_model)
+
+    tf.keras.models.save_model(model, latest_model)
+    make_archive(latest_model, 'zip', latest_model)
+
+    print(f"\nok, saved to {latest_model} ({round(get_size(latest_model) / 1000000, 1)} MBs).")
+
+
+@contextlib.contextmanager
+def suppress_stdout():
+    with open(os.devnull, "w") as devnull:
+        sys.stdout = devnull
+        sys.stderr = devnull
+        try:
+            yield
+        finally:
+            sys.stdout = sys.__stdout__
+            sys.stderr = sys.__stderr__
+
+
+if not exists("dataset"):
     print("dataset not found. (folder name should be 'dataset' and in this directory)")
     exit()
 
-# noinspection PyUnboundLocalVariable
-train_ds = tf.keras.utils.image_dataset_from_directory(
-    data_dir,
-    validation_split=0.2,
-    subset="training",
-    seed=123,
-    image_size=(img_height, img_width),
-    batch_size=batch_size)
-val_ds = tf.keras.utils.image_dataset_from_directory(
-    data_dir,
-    validation_split=0.2,
-    subset="validation",
-    seed=123,
-    image_size=(img_height, img_width),
-    batch_size=batch_size)
+data_dir = pathlib.Path("dataset")
+
+clean = True if input("would you like to clean your dataset? (y or n): ").lower() == "y" else False
+print("\n", end="")
+
+
+if clean:
+    files = 0
+    total = sum([len(files) for r, d, files in os.walk("dataset")])
+
+    for dirpath, dirnames, filenames in os.walk("dataset"):
+        for f in filenames:
+            files += 1
+
+            fp = os.path.join(dirpath, f)
+            path = pathlib.Path(fp)
+            new_path = os.path.join(path.parent if "dataset" not in path.parent.name else path.name)
+
+            if what(fp) is None:
+                move(fp, f"invalid_files/{new_path}")
+
+            print(f"\rcleaning dataset.. ({files}/{total})", end="")
+    print("\rcleaning dataset..done")
+
+print("loading dataset..", end="")
+
+with suppress_stdout():
+    # noinspection PyUnboundLocalVariable
+    train_ds = tf.keras.utils.image_dataset_from_directory(
+        data_dir,
+        validation_split=0.2,
+        subset="training",
+        seed=123,
+        image_size=(img_height, img_width),
+        batch_size=batch_size)
+    print(f"\rloading dataset.. (1/2)", end="")
+    val_ds = tf.keras.utils.image_dataset_from_directory(
+        data_dir,
+        validation_split=0.2,
+        subset="validation",
+        seed=123,
+        image_size=(img_height, img_width),
+        batch_size=batch_size)
+    print("\rloading dataset..done")
 
 class_names = train_ds.class_names
-print(f"found {len(class_names)} labels. {' and '.join(class_names)}.")
+print("done\n")
+
+print(f"found {len(class_names)} labels from dataset.\n")
 
 if train:
     normalization_layer = layers.Rescaling(1. / 255)
     normalized_ds = train_ds.map(lambda x, y: (normalization_layer(x), y))
     image_batch, labels_batch = next(iter(normalized_ds))
-    first_image = image_batch[0]
 
     num_classes = len(class_names)
 
@@ -69,47 +147,10 @@ if train:
                               input_shape=(img_height,
                                            img_width,
                                            3)),
-            layers.RandomRotation(0.2),
-            layers.RandomZoom(0.2),
+            layers.RandomRotation(uniform(0.1, 0.4)),
+            layers.RandomZoom(uniform(0.1, 0.4)),
         ]
     )
-
-
-    def get_size(start_path='.'):
-        total_size = 0
-        for dirpath, dirnames, filenames in os.walk(start_path):
-            for f in filenames:
-                fp = os.path.join(dirpath, f)
-                # skip if it is a symbolic link
-                if not os.path.islink(fp):
-                    total_size += os.path.getsize(fp)
-
-        return total_size
-
-
-    def before_exit():
-        save = True if input("would you like to save your trained model? (y or n): ") \
-                           .lower() == "y" else False
-        if save:
-            if exists(old_model) and exists(latest_model):
-                rmtree(old_model)
-                rename(latest_model, old_model)
-            elif exists(latest_model):
-                rename(latest_model, old_model)
-
-            tf.keras.models.save_model(model, latest_model)
-            make_archive(latest_model, 'zip', latest_model)
-
-            print(f"\nok, saved to {latest_model} ({round(get_size(latest_model) / 1000000, 1)} MBs).")
-
-
-    if exists(latest_model):
-        load = True if input("would you like to load your latest saved model to continue training? (y or n): ") \
-                           .lower() == "y" else False
-        if load:
-            print("ok, loading model..", end="")
-            model = tf.keras.models.load_model(latest_model, compile=False)
-            print("done")
 
     model = Sequential([
         data_augmentation,
@@ -126,28 +167,36 @@ if train:
         layers.Dense(num_classes, name="outputs")
     ])
 
+    if exists(latest_model):
+        load = True if input(
+            "would you like to load your latest saved model to continue training? (y or n): ").lower() == "y" else False
+        if load:
+            print("loading model..", end="")
+            model = tf.keras.models.load_model(latest_model, compile=False)
+            print("done")
+
     model.compile(optimizer='adam',
                   loss=tf.keras.losses.SparseCategoricalCrossentropy(from_logits=True),
                   metrics=['accuracy'])
 
     try:
         EPOCHS = int(input("how many epochs do you want to train for? "))
-        if EPOCHS > 0:
-            print(f"\nok, starting training for {EPOCHS} epochs")
-        else:
+        if EPOCHS < 0:
             print("cannot train for 0 epochs, falling back to 5 epochs.\n")
             EPOCHS = 5
-            print(f"starting training for {EPOCHS} epochs")
     except ValueError:
         print("not a number, falling back to 5 epochs.\n")
         EPOCHS = 5
-        print(f"starting training for {EPOCHS} epochs")
 
-    history = model.fit(
-        train_ds,
-        validation_data=val_ds,
-        epochs=EPOCHS
-    )
+    for i in range(EPOCHS):
+        print("", end="\n\n" if i == 0 else "\n")
+
+        print("Epoch 1: ")
+        history = model.fit(
+            train_ds,
+            validation_data=val_ds,
+            epochs=1
+        )
 
     before_exit()
 else:
@@ -179,7 +228,8 @@ else:
                 "This image most likely belongs to {} with a {:.2f}% confidence."
                 .format(class_names[np.argmax(score)], 100 * np.max(score))
             )
-        except FileNotFoundError:
+        except (FileNotFoundError, OSError):
+            print("file not found\n")
             pass
         except KeyboardInterrupt:
             exit()
